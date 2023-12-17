1d9cb42c9aaa48ed17e09399ff81d4542b08d910
TheBarto <agftotg10@gmail.com>
2023-12-17 20:03:54 +0100

    Clear the code, make some static internal functions and eliminate some magic numbers

--- reader.c
+++ reader.c
@@ -1,71 +1,41 @@
 #include "reader.h"
-
 #include <errno.h>
 #include <stdio.h>
-#if 0
-uint8_t open_file(FileReader* fr, uint8_t* file_name)
+
+static uint32_t parse_white_spaces(uint8_t* file, uint32_t size)
 {
-    memset(fr, 0, sizeof(FileReader));
-    fr->file_desc = open((char *)file_name, O_RDONLY);
-    if(fr->file_desc == -1)
-    {
-       printf("Errno(%d) - %s\n", errno, strerror(errno)); 
-        return -1;
+    uint8_t *ptr1, *ptr2;
+    ptr1 = ptr2 = file;
+
+    uint8_t state = 0;
+    //Adjust file to the first valid vlaue
+    while(*ptr2 != 0) {
+    if(state == 0) {
+        if(*ptr2 == '/') {
+            if(*(ptr2+1) == '*') state = 2;
+            else if(*(ptr2+1) == '/') state = 1;
+        } else if((*ptr2 >= 0x21) && (*ptr2<=0x7E)) {
+            *ptr1 = *ptr2;
+            ptr1++;
+        }
+    } else if(((state == 1) &&
+              (*ptr2 == '\n')) ||
+              ((state == 2) &&
+               (*ptr2 == '*') &&
+               (*(ptr2+1) == '/'))){
+        if(state == 2)
+            ptr2++;
+        state = 0;
     }
-
-    fr->total_file_size = lseek(fr->file_desc, 0, SEEK_END);
-    lseek(fr->file_desc, 0, SEEK_SET);
-
-    fr->file_ptr = calloc(fr->total_file_size, sizeof(uint8_t));
-    if(!fr->file_ptr)
-        return -1;
-
-    //Error control, close the file
-    //if(fr->total_file_size != 
-    //        read(fr->file_desc, fr->file_ptr, fr->total_file_size))
-    //    return -1;
-    printf("... %lu\n", read(fr->file_desc, fr->file_ptr, fr->total_file_size));
-
-    fr->separator = ',';
-    //fr->separator[1] = 0;
-    
-    fr->line_start = ':';
-
-    //Need to add new end (0) and re-ajust the size
-    fr->file_size_adj = parse_white_spaces(fr->file_ptr, fr->total_file_size);
-    fr->ptr_read = fr->file_ptr;
-
-    return 0;
-}
-#endif
-
-uint8_t open_file(FileReader* fr, uint8_t* file_name)
-{
-    memset(fr, 0, sizeof(FileReader));
-    fr->file_desc = open((char *)file_name, O_RDONLY);
-    if(fr->file_desc == -1)
-    {
-       printf("Errno(%d) - %s\n", errno, strerror(errno)); 
-        return -1;
+    ptr2++;
     }
-    //if(fr->total_file_size != 
-    //        read(fr->file_desc, fr->file_ptr, fr->total_file_size))
-    //    return -1;
-    printf("... %lu\n", read(fr->file_desc, fr->file_lec, 512));
+    *ptr1 = 0;
 
-    fr->separator = ',';
-    fr->line_start = ':';
-
-    fr->ptr_read = &fr->file_lec[0];
-    //Need to add new end (0) and re-ajust the size
-    parse_white_spaces(fr->file_lec, 512);
-
-    return 0;
+    return (ptr1-file);
 }
-
 
 //Error if the number starts with 0, like 001
-int32_t parse_value(uint8_t* ptr_file, uint32_t size)
+static int32_t parse_value(uint8_t* ptr_file, uint32_t size)
 {
     uint32_t val = 0;
 
@@ -80,7 +50,7 @@ int32_t parse_value(uint8_t* ptr_file, uint32_t size)
     return val;
 }
 
-int32_t parse_hex_value(uint8_t* ptr_file, uint32_t size)
+static int32_t parse_hex_value(uint8_t* ptr_file, uint32_t size)
 {
     uint32_t val = 0;
 
@@ -100,7 +70,7 @@ int32_t parse_hex_value(uint8_t* ptr_file, uint32_t size)
     return val;
 }
 
-void parse_value_readed(uint8_t* ptr_file, uint32_t size, uint32_t* dst_arr)
+static void parse_value_readed(uint8_t* ptr_file, uint32_t size, uint32_t* dst_arr)
 {
     //If find the 0x value, transform hexadecimal value 
     if((*ptr_file == '0') && 
@@ -112,47 +82,35 @@ void parse_value_readed(uint8_t* ptr_file, uint32_t size, uint32_t* dst_arr)
     return;
 }
 
-#if 0
-//Be carefull with the jump lines values, because it adds more undesired length 
-int32_t read_line(FileReader* fr, uint8_t* line_start, uint32_t* dst_arr)
+uint8_t open_file(FileReader* fr, uint8_t* file_name)
 {
-    //uint8_t* ptr_file = strip_values(fr->file_ptr, fr->total_file_size);
-    //parse_white_spaces(&ptr_file); //strip better?
-    uint8_t* ptr_file = fr->ptr_read;
-
-    uint8_t i = 0;
-    //Search the line start
-    while((*ptr_file != fr->line_start))
-        ptr_file++;
-    /*line_start[i] != 0) {
-        (*ptr_file == line_start[i])?i++:(i=0);
-        ptr_file++;
-    }*/
-
-    ptr_file++;//Advance one to pass the ':'
-    uint8_t* ptr_aux_file = ptr_file;
-    uint32_t tot_vals = 0;
-    while((*ptr_file != 0) && 
-            (*ptr_file!= ':')) {
-        while((*ptr_aux_file != fr->separator) && 
-              (*ptr_aux_file != fr->line_start) && 
-              (*ptr_aux_file != 0))
-                ptr_aux_file++;
-        parse_value_readed(ptr_file, (ptr_aux_file - ptr_file), &dst_arr[tot_vals]);
-        tot_vals++;
-        if(*ptr_aux_file == ':') break;
-        ptr_aux_file++;
-        ptr_file = ptr_aux_file;
+    memset(fr, 0, sizeof(FileReader));
+    fr->file_desc = open((char *)file_name, O_RDONLY);
+    if(fr->file_desc == -1)
+    {
+       printf("Errno(%d) - %s\n", errno, strerror(errno)); 
+        return -1;
     }
-    fr->ptr_read = ptr_file;
+    printf("Readed: ... %lu\n", read(fr->file_desc, fr->file_lec, ONE_TIME_READ));
 
-    return tot_vals;
+    fr->separator = ',';
+    fr->line_start = ':';
+
+    fr->ptr_read = &fr->file_lec[0];
+    //Need to add new end (0) and re-ajust the size
+    parse_white_spaces(fr->file_lec, ONE_TIME_READ);
+
+    return 0;
 }
-#endif
 
+//PROBLEM WITH READ, BLOCKS, BUT DONT NO WHY
 //Be carefull with the jump lines values, because it adds more undesired length 
 int32_t read_line(FileReader* fr, uint8_t* line_start, uint32_t* dst_arr)
 {
+    //If we got a NULL character, finish
+    if(!*fr->ptr_read)
+        return 0;
+
     uint8_t* ptr_file = fr->ptr_read;
     
     //Search the line start
@@ -175,10 +133,19 @@ int32_t read_line(FileReader* fr, uint8_t* line_start, uint32_t* dst_arr)
             ptr_aux_file++;
             ptr_file = ptr_aux_file;
         } else if(*ptr_aux_file == 0) {
-            memcpy(&fr->file_lec[0], ptr_aux_file, (512-(ptr_aux_file-ptr_file)));
+            memcpy(&fr->file_lec[0], ptr_file, (ptr_aux_file-ptr_file));
             ptr_aux_file = &fr->file_lec[(ptr_aux_file-ptr_file)];
             ptr_file = &fr->file_lec[0];
-            parse_white_spaces(ptr_aux_file, read(fr->file_desc, ptr_aux_file, 512));
+            uint16_t readed_vals = read(fr->file_desc, ptr_aux_file, ONE_TIME_READ);
+            if(readed_vals > 0)
+                parse_white_spaces(ptr_aux_file, readed_vals);
+            else {
+                parse_value_readed(ptr_file, (ptr_aux_file - ptr_file), &dst_arr[tot_vals]);
+                ptr_file = ptr_aux_file;
+                tot_vals++;
+                *ptr_aux_file = 0;
+                break;
+            }
         } else if(*ptr_aux_file == fr->line_start) {
             parse_value_readed(ptr_file, (ptr_aux_file - ptr_file), &dst_arr[tot_vals]);
             ptr_file = ptr_aux_file;
@@ -189,94 +156,4 @@ int32_t read_line(FileReader* fr, uint8_t* line_start, uint32_t* dst_arr)
     fr->ptr_read = ptr_file;
 
     return tot_vals;
-}
-
-/*void load_new_file_info(FileReader* fr, uint8_t* ptr_aux_file, uint8_t) {
-    
-    memcpy(&fr->file_anal[0], fr->ptr_file, (ptr_file_aux - fr->ptr_read));
-    printf("... %lu\n", read(fr->file_desc, fr->file_read, fr->total_file_size));
-    memcpy(fr->file_anal[(ptr_file_aux - fr->ptr_read)], fr->file_lec, (512-(ptr_file_aux - fr->ptr_read))); 
-    fr->ptr_file_aux = &fr->file_anal[(ptr_file_aux - fr->ptr_read)];
-    fr->ptr_read = fr->file_anal[0];
-
-    return;
-}*/
-
-uint8_t* strip_values(uint8_t* file, uint32_t ssize)
-{
-    uint8_t* ptr = file;
-    while((*ptr == ' ') || (*ptr == '\t'))
-       ptr++; 
-
-    return ptr;
-}
-
-#if 0
-uint32_t parse_white_spaces(uint8_t* file, uint32_t total_size)
-{
-    uint8_t* ptr1, *ptr2;
-    uint8_t aux = 0;
-    ptr1 = ptr2 = file;
-
-    while(*ptr2 != 0) {
-        //Advance to the first desired position
-        /*if(((*ptr2 == 0x7E) ||
-           (*ptr2 < 0x21)) &&
-          (aux == 0)) {
-            aux = 1;
-            ptr1 = ptr2;
-        }*/
-
-        if(/*(aux == 1) &&*/ (*ptr2 >= 0x21) &&
-           (*ptr2 <= 0x7E)) { 
-            *ptr1 = *ptr2;
-            ptr1++;
-        }
-        ptr2++;
-    }
-
-    ptr1++;
-    *ptr1=0;
-
-    //printf("Size: %d, New string readed: %s\n", (ptr1-file), file);
-
-	return (ptr1-file);
-}
-#endif
-
-uint32_t parse_white_spaces(uint8_t* file, uint32_t size)
-{
-    uint8_t *ptr1, *ptr2;
-    ptr1 = ptr2 = file;
-
-    uint8_t state = 0;
-    //Adjust file to the first valid vlaue
-    while(*ptr2 != 0) {
-    if(state == 0) {
-        if(*ptr2 == '/') {
-            if(*(ptr2+1) == '*') state = 2;
-            else if(*(ptr2+1) == '/') state = 1;
-        } else if((*ptr2 >= 0x21) && (*ptr2<=0x7E)) {
-            *ptr1 = *ptr2;
-            /*if(*ptr2 == ':')
-                fr->separators[i] = ptr1-file;
-            if(*ptr2 == ',')
-                fr->data[j] = ptr1-file;*/
-            ptr1++;
-        }
-    } else if(((state == 1) && 
-              (*ptr2 == '\n')) ||
-              ((state == 2) && 
-               (*ptr2 == '*') && 
-               (*(ptr2+1) == '/'))){
-        if(state == 2)
-            ptr2++;
-        state = 0;
-    }
-    ptr2++;
-    }
-    //ptr1++;
-    *ptr1 = 0;
-
-    return (ptr1-file);
 }
--- reader.h
+++ reader.h
@@ -7,9 +7,11 @@
 #include <unistd.h>
 #include <string.h>
 
+#define ONE_TIME_READ ((uint16_t) 512)
+
 typedef struct {
     int8_t file_desc;
-    uint8_t file_lec[1024];
+    uint8_t file_lec[(ONE_TIME_READ*2)];
     uint8_t* ptr_read; //Ptr to continue read the data
     uint8_t separator; /* Separator between elements*/
     uint8_t line_start; /* Line start */
@@ -19,16 +21,6 @@ typedef struct {
 
 uint8_t open_file(FileReader* fr, uint8_t* file_name);
 
-int32_t parse_value(uint8_t* ptr_file, uint32_t size);
-
-int32_t parse_hex_value(uint8_t* ptr_file, uint32_t size);
-
-void parse_value_readed(uint8_t* ptr_file, uint32_t size, uint32_t* dst_arr);
-
 int32_t read_line(FileReader* fr, uint8_t* line_start, uint32_t* dst_arr);
-
-uint32_t parse_white_spaces(uint8_t* file, uint32_t total_size);
-
-uint8_t* strip_values(uint8_t* file, uint32_t ssize);
 
 #endif //READER_H
